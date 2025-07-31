720c68e39fe4c23b0cbdc05fff98f1825f92a209
pacific-pvt <pvt05710022@pvt.pacific-ind.com.tw>
2025-07-31 15:35:23 +0800

    Update SheetData.js

--- javascript/SheetData.js
+++ javascript/SheetData.js
@@ -1,19 +1,23 @@
-export const url = 'https://script.google.com/macros/s/AKfycbzs-fs-1ngOvhQu2bsOeNuaxuT7kUxKm8PSFlCSg_gcb5BuN_Ak1iNOO4maQ3UTRbd1kw';
-
+export const url = 'https://script.google.com/macros/s/AKfycbzs-fs-1ngOvhQu2bsOeNuaxuT7kUxKm8PSFlCSg_gcb5BuN_Ak1iNOO4maQ3UTRbd1kw/exec';
 
 export async function fetchData(formData) {
-  const response = await fetch(url, {
-    redirect: 'follow',
-    method: 'POST', // 設定 HTTP 方法為 POST
-    body: JSON.stringify(formData), // 將資料轉換為 JSON 格式並放入 body
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' // 設定 Content-Type
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', // 用 JSON 傳送
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-    //body: `data=${encodeURIComponent(JSON.stringify(formData))}`
-  });
 
-  const data = await response.json(); // 解析 JSON
-  return data; // 直接返回資料，不做任何處理
+    const data = await response.json(); // 回傳 JSON
+    return data;
+  } catch (error) {
+    console.error('Fetch failed:', error);
+    throw error;
+  }
 }
-
-
