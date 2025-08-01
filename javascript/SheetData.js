82ba6ea54d39b0341c7cb4aa6682e7c40a44e098
pacific-pvt <pvt05710022@pvt.pacific-ind.com.tw>
2025-08-01 14:31:26 +0800

    Update SheetData.js

--- javascript/SheetData.js
+++ javascript/SheetData.js
@@ -1,18 +1,21 @@
 export const url = 'https://script.google.com/macros/s/AKfycbxt78kqhSqKGmZJSGdU3YcDkk96fX-VnMGB1hG-MafB3NcBPrEkfb8bLZy8_32C9VMSQw/exec';
 
-
 export async function fetchData(formData) {
-  const response = await fetch(url, {
-    redirect: 'follow',
-    method: 'POST', // 設定 HTTP 方法為 POST 
+  // 将 formData 转换为查询字符串
+  const params = new URLSearchParams(formData).toString();
+
+  // 修改为 GET 请求，将参数附加到 URL 后
+  const response = await fetch(`${url}?${params}`, {
+    method: 'GET',
     headers: {
-        //'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' // 設定 Content-Type
-        //'Content-Type': 'text/plain;charset=utf-8'
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-     body: JSON.stringify(formData)// 將資料轉換為 JSON 格式並放入 body      
+      'Content-Type': 'application/json;charset=utf-8' // 你仍然可以设置这个头，但它在 GET 请求中并不必要
+    }
   });
 
+  if (!response.ok) {
+    throw new Error('网络请求失败');
+  }
+
   const data = await response.json(); // 解析 JSON
-  return data; // 直接返回資料，不做任何處理
+  return data; // 返回数据
 }
