bc93349ec3d4c27f03c19fbd4b4ab3f035146b0c
pacific-pvt <pvt05710022@pvt.pacific-ind.com.tw>
2025-08-01 08:25:28 +0800

    Update SheetData.js

--- javascript/SheetData.js
+++ javascript/SheetData.js
@@ -1,15 +1,13 @@
 export const url = 'https://script.google.com/macros/s/AKfycbxt78kqhSqKGmZJSGdU3YcDkk96fX-VnMGB1hG-MafB3NcBPrEkfb8bLZy8_32C9VMSQw/exec';
 
-
 export async function fetchData(formData) {
   const response = await fetch(url, {
     redirect: 'follow',
     method: 'POST', // 設定 HTTP 方法為 POST
-    body: JSON.stringify(formData), // 將資料轉換為 JSON 格式並放入 body
     headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' // 設定 Content-Type
-    }
-    //body: `data=${encodeURIComponent(JSON.stringify(formData))}`
+      'Content-Type': 'application/json;charset=utf-8' // 設定 Content-Type
+    }, // 注意逗號
+    body: JSON.stringify(formData) // 將資料轉換為 JSON 格式並放入 body
   });
 
   const data = await response.json(); // 解析 JSON
