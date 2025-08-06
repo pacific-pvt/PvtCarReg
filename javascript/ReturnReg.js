75ac67b7d93426610489c46905fa5554d3e29565
pacific-pvt <pvt05710022@pvt.pacific-ind.com.tw>
2025-08-06 09:10:06 +0800

    Update ReturnReg.js

--- javascript/ReturnReg.js
+++ javascript/ReturnReg.js
@@ -114,7 +114,7 @@ async function ReturnReg() {
         'enddate': backreturn.value.split(" ")[0], // 返回日期
         'endtime': backreturn.value.split(" ")[1], // 返回時間
         'gas': backgas.value,//有無加油
-        'actualReturnDate': backreturnTime.value.split("T")[0],//實際返回日期
+        'actualReturnDate': backreturnDate.value.split("T")[0],//實際返回日期
         'actualReturnTime': backreturnTime.value.split("T")[1],//實際返回時間
         
         'code': '4'
@@ -142,5 +142,6 @@ document.getElementById("btn-Cancel").addEventListener('click', function (event)
     myModal.hide();
     document.location.reload();
 })
+
 
 
